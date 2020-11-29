ba1d38f5aa52b65cb4178da38ece08a27db66d45
vladimirvinicius <vladimirvinicius@gmail.com>
2020-11-29 17:01:57 -0300

    Customização de Usuário do Django

--- pypro/base/admin.py
+++ pypro/base/admin.py
@@ -6,7 +6,6 @@ from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import (
     AdminPasswordChangeForm, UserChangeForm, UserCreationForm,
 )
-from django.contrib.auth.models import Group
 from django.core.exceptions import PermissionDenied
 from django.db import router, transaction
 from django.http import Http404, HttpResponseRedirect
@@ -17,11 +16,11 @@ from django.utils.html import escape
 from django.utils.translation import gettext, gettext_lazy as _
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
+
 from pypro.base.models import User
 
 csrf_protect_m = method_decorator(csrf_protect)
 sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
-
 
 
 @admin.register(User)
