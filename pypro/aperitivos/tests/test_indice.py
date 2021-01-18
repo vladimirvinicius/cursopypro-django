eef79f17d78fee38e87afb16c8185008dcaf2ffb
vladimirvinicius <vladimirvinicius@gmail.com>
2021-01-18 16:37:10 -0300

    Criado modelos para adição de vídeos

--- pypro/aperitivos/tests/test_indice.py
+++ pypro/aperitivos/tests/test_indice.py
@@ -1,10 +1,17 @@
 import pytest
 from django.urls import reverse
 from pypro.django_assertions import assert_contains
+from model_bakery import baker
+from pypro.aperitivos.models import Video
 
 
 @pytest.fixture
-def resp(client):
+def videos(db):
+    return baker.make(Video, 2)
+
+
+@pytest.fixture
+def resp(client, videos):
     return client.get(reverse('aperitivos:indice'))
 
 
@@ -12,24 +19,12 @@ def test_status_code_indice(resp):
     assert resp.status_code == 200
 
 
-@pytest.mark.parametrize(
-    'titulo',
-    [
-     'Vídeo Aperitivo: Motivação',
-     'Instalação'
-    ]
-)
-def test_titulo_video_indice(resp, titulo):
-    assert_contains(resp, titulo)
+def test_titulo_video(resp, videos):
+    for video in videos:
+        assert_contains(resp, video.titulo)
 
 
-@pytest.mark.parametrize(
-    'slug',
-    [
-     'motivacao',
-     'instalacao'
-    ]
-)
-def test_titulo_link_indice(resp, slug):
-    video_link = reverse('aperitivos:video', args=(slug,))
-    assert_contains(resp, f'href="{video_link}"')
+def test_titulo_link_indice(resp, videos):
+    for video in videos:
+        video_link = reverse('aperitivos:video', args=(video.slug,))
+        assert_contains(resp, f'href="{video_link}"')
--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -1,20 +1,36 @@
 import pytest
 from django.urls import reverse
 from pypro.django_assertions import assert_contains
+from pypro.aperitivos.models import Video
+from model_bakery import baker
 
 
 @pytest.fixture
-def resp(client):
-    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+def video(db):
+    return baker.make(Video)
+
+
+@pytest.fixture
+def resp(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug,)))
+
+
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(f"{video.slug}_video_nao_encontrado",)))
+
+
+def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_titulo_video(resp):
-    assert_contains(resp, '<h1 class="mt-4 mb-3">Vídeo Aperitivo: Motivação</h1>')
+def test_titulo_video(resp, video):
+    assert_contains(resp, video.titulo)
 
 
-def test_conteudo_video(resp):
-    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/496273362"')
+def test_conteudo_video(resp, video):
+    assert_contains(resp, f'<iframe src="https://player.vimeo.com/video/{video.vimeo_id}"')
